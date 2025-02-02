cf165d47f98b56f2f4fd4c6e08626cedc39d1c59
berki-devs <berki.devs@gmail.com>
2025-02-02 19:10:59 +0500

    Initial commit

---
+++ src/test/java/devs/berki/cellnetbackend/CellnetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.berki.cellnetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellnetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
